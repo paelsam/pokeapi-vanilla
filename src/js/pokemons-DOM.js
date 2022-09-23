0875a867a080f21b84faac8b4c3d848e1b3db320
Elkin Panameño <elkinsamir643@gmail.com>
2022-09-23 18:25:39 -0500

    Interfaz Mejoradas y nuevos colores por cada tipo de pokemon

--- src/js/pokemons-DOM.js
+++ src/js/pokemons-DOM.js
@@ -1,21 +1,30 @@
 import { pokemons } from './http-provider';
+// CSS
+import '../css/componets.css';
 
 const $ = (element) => document.querySelector(element);
 const sectionPokemons = $('#pokemons');
+const btnAntes = $('#antes');
+const btnDespues = $('#despues');
 let pokemonList;
+let offset = 0;
 
-console.log(sectionPokemons);
 
 
 const createHTML = ({ id, name, types: [type1, type2], sprite }) => {
 
+        // Previous disabled if offset is equal to 0
+        btnAntes.disabled = offset === 0 ? true : false;
+
         const html = `
         <div class="card m-1" style="width: 10rem;">
-            <img src="${sprite}" class="card-img-top" alt="${name}">
+            <img src="${sprite}" class="${type1.toLowerCase()}" loading="lazy" alt="${name}">
             <div class="card-body text-center">
-                <p class="card-text bold">${id}</p>
-                <p class="card-text">${ type1 } ${ type2 ? `- ${ type2 }` : '' }</p>
-                <p class="card-text">${name}</p>
+                <p class="card-text bold">${id}: ${name}</p>
+                <p class="card-text">
+                    <span class="${type1.toLowerCase()} bold">${type1}</span>  
+                    <span class="${type2 ? type2.toLowerCase() : ''} bold"> ${type2 ? `${type2}` : ''}</span>  
+                </p>
             </div>
         </div>`
 
@@ -23,13 +32,34 @@ const createHTML = ({ id, name, types: [type1, type2], sprite }) => {
 
     div.innerHTML = html;
 
-    sectionPokemons.append( div.firstElementChild  );
+    sectionPokemons.append(div.firstElementChild);
 }
 
+const events = () => {
 
-async function init () {
-    pokemonList = await pokemons( 0, 10);
-    pokemonList.forEach( (pokemon) => createHTML( pokemon ) );
+
+    btnAntes.addEventListener("click", () => {
+        sectionPokemons.innerHTML = ``;
+        offset -= 10;
+        displayPokemons(offset);
+    })
+
+    btnDespues.addEventListener("click", () => {
+        sectionPokemons.innerHTML = ``;
+        offset += 10;
+        displayPokemons(offset);
+    })
+
+}
+
+const displayPokemons = async (offset) => {
+    pokemonList = await pokemons(offset, 10);
+    pokemonList.forEach((pokemon) => createHTML(pokemon));
+}
+
+function init() {
+    displayPokemons(offset);
+    events();
 }
 
 
